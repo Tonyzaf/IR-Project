f1bb9a109c0d968113a02ee6d497e567bf7c56fa
Antonis Zafeirelis <zaftony@hotmail.gr>
2019-12-16 14:16:28 +0200

    Output Fixed

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 if es.ping():print('connection success!')
 else:print('ping failed')
  
-name = raw_input('Insert The Title Of The Movie:\n')
+UserID = int(input('Insert Your UserID: '))
+print(UserID)
+name = raw_input('Insert The Title Of The Movie: ')
 
 query_body = {
   "query": {
@@ -30,15 +32,17 @@ query_body = {
     }
   }
 }
-
-result = es.search(index="movies", body=query_body,size=999)
-all_hits = result['hits']['hits']
-
-
-print ("total hits:", len(result["hits"]["hits"]))
-
-for num, doc in enumerate(all_hits):
-    print ("DOC ID:", doc["_id"], "--->", doc, type(doc), "\n")
-    for key, value in doc.items():
-        print (key, "-->", value)
-    print ("\n\n")
+res = es.search(index="movies", body=query_body, size=10)
+with open('results.csv', 'w') as f: 
+    header_present  = False
+    for doc in res['hits']['hits']:
+        my_dict = doc['_source'] 
+        if not header_present:
+            w = csv.DictWriter(f, my_dict.keys())
+            w.writeheader()
+            header_present = True
+        w.writerow(my_dict)
+with open('results.csv', 'r') as f: 
+    reader = csv.reader(f)
+    for row in reader:
+      print('Title:', row[2] ,'ID:', row[1],'Genres:',  row[0])
